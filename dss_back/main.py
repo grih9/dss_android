0cd160fb55ed4de9f2b93c74358a9191e8bf5c40
Grigoriy Tolstikov <tolstikov.gn@edu.spbstu.ru>
2023-11-19 15:32:59 +0300

    update main file

--- dss_back/main.py
+++ dss_back/main.py
@@ -16,7 +16,7 @@ from core.mechanisms import (
     calculate_tournament,
     calculate_tournament_results_by_variant
 )
-from models.result_models import TournamentResult, KMaxResult, DominanceResult, LockResult
+from models.result_models import TournamentResult, KMaxResult, DominanceResult, LockResult, KMaxOptimalResult
 
 
 # async def runner():
@@ -30,7 +30,6 @@ def runner():
         choice_function_path="./input_data/test_data/choice_function.csv",
         weight_coefficients_path="./input_data/test_data/weight_coefficients.csv"
     )
-
 
 
     df_binary = calculate_binary_relations(df)
@@ -62,21 +61,21 @@ def runner():
         df_k_max_by_variant
     )
 
-    writer = Writer()
-    writer(
-        dir_to_save="./decision_support_system/output_data",
-        raw_df=df,
-        binary_rel_df=df_binary,
-        dominance_df=df_dominance,
-        dominance_df_by_variant=df_dominance_by_variant,
-        lock_df=df_lock,
-        lock_df_by_variant=df_lock_by_variant,
-        tournament_df=df_tournament,
-        tournament_df_by_variant=df_tournament_by_variant,
-        k_max_df=df_k_max,
-        k_max_df_by_variant=df_k_max_by_variant,
-        summary_df=df_summary
-    )
+    # writer = Writer()
+    # writer(
+    #     dir_to_save="./decision_support_system/output_data",
+    #     raw_df=df,
+    #     binary_rel_df=df_binary,
+    #     dominance_df=df_dominance,
+    #     dominance_df_by_variant=df_dominance_by_variant,
+    #     lock_df=df_lock,
+    #     lock_df_by_variant=df_lock_by_variant,
+    #     tournament_df=df_tournament,
+    #     tournament_df_by_variant=df_tournament_by_variant,
+    #     k_max_df=df_k_max,
+    #     k_max_df_by_variant=df_k_max_by_variant,
+    #     summary_df=df_summary
+    # )
 
 
 def main():
@@ -87,18 +86,18 @@ if __name__ == '__main__':
      main()
 
 from typing import Union
-from models.raw_data import RawDataframe, RawDataframeModel
+from models.raw_data import RawDataframe
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", tags=["GET Methods"])
 def read_root():
     return {"info": "This is DSS backend made with FastAPI"}
 
 
-@app.get("/help")
+@app.get("/help", tags=["GET Methods"])
 def get_routes():
     return {
         "calculate_tournament": "POST /mechanism/tournament",
@@ -109,41 +108,42 @@ def get_routes():
     }
 
 
-@app.post("/mechanism/tournament")
-def calculate_tournament_api(*, raw_data: RawDataframeModel):
+@app.post("/mechanism/tournament", tags=["POST Methods"])
+def calculate_tournament_api(*, raw_data: RawDataframe) -> TournamentResult:
     df_binary = calculate_binary_relations(raw_data)
     df_tournament = calculate_tournament(df_binary)
     df_tournament_by_variant = calculate_tournament_results_by_variant(df_tournament)
     return TournamentResult(summary_result=df_tournament, result_by_variant=df_tournament_by_variant)
 
 
-@app.post("/mechanism/kmax")
-def calculate_kmax(*, raw_data: RawDataframeModel):
-    df_binary = calculate_binary_relations(raw_data)
-    df_kmax = calculate_k_max_options(df_binary)
-    df_kmax_by_variant = calculate_k_max_results_by_variant(df_kmax)
-    return KMaxResult(summary_result=df_kmax, result_by_variant=df_kmax_by_variant)
-
-
-@app.post("/mechanism/kmax_optimal")
-def calculate_kmax_optimal(*, raw_data: RawDataframeModel):
-    df_binary = calculate_binary_relations(raw_data)
-    df_kmax = calculate_k_max_options(df_binary)
-    df_kmax_by_variant = calculate_k_max_results_by_variant(df_kmax)
-    return KMaxResult(summary_result=df_kmax, result_by_variant=df_kmax_by_variant)
-
-
-@app.post("/mechanism/locking")
-def calculate_locking(*, raw_data: RawDataframeModel):
+@app.post("/mechanism/locking", tags=["POST Methods"])
+def calculate_locking(*, raw_data: RawDataframe) -> LockResult:
     df_binary = calculate_binary_relations(raw_data)
     df_lock = calculate_lock(df_binary)
     df_lock_by_variant = calculate_lock_results_by_variant(df_lock)
     return LockResult(summary_result=df_lock, result_by_variant=df_lock_by_variant)
 
 
-@app.get("/mechanism/dominance")
-def calculate_dominance_f(*, raw_data: RawDataframeModel):
+@app.post("/mechanism/dominance", tags=["POST Methods"])
+def calculate_dominance_f(*, raw_data: RawDataframe) -> DominanceResult:
     df_binary = calculate_binary_relations(raw_data)
     df_dominance = calculate_dominance(df_binary)
-    df_dominance_by_variant = calculate_lock_results_by_variant(df_lock)
+    df_dominance_by_variant = calculate_dominance_results_by_variant(df_dominance)
     return DominanceResult(summary_result=df_dominance, result_by_variant=df_dominance_by_variant)
+
+
+@app.post("/mechanism/kmax", tags=["POST Methods"])
+def calculate_kmax(*, raw_data: RawDataframe) -> KMaxResult:
+    df_binary = calculate_binary_relations(raw_data)
+    df_kmax = calculate_k_max_options(df_binary)
+    df_kmax_by_variant = calculate_k_max_results_by_variant(df_kmax)
+    return KMaxResult(summary_result=df_kmax, result_by_variant=df_kmax_by_variant, result_by_variant_optimal=df_kmax_by_variant)
+
+
+@app.post("/mechanism/kmax_optimal", tags=["POST Methods"])
+def calculate_kmax_optimal(*, raw_data: RawDataframe) -> KMaxOptimalResult:
+    df_binary = calculate_binary_relations(raw_data)
+    df_kmax = calculate_k_max_options(df_binary)
+    df_kmax_by_variant = calculate_k_max_results_by_variant(df_kmax)
+    return KMaxOptimalResult(summary_result=df_kmax, result_by_variant=df_kmax_by_variant, result_by_variant_optimal=df_kmax_by_variant)
+
